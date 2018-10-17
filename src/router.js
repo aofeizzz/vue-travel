b6129a530f902598a7cfed3f7e245aa08fac68e4
aofeizzz <228589060@qq.com>
2018-10-17 10:26:36 +0800

    修改首页和路由

--- src/router.js
+++ src/router.js
@@ -1,25 +1,24 @@
-import Vue from 'vue'
+import Vue    from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home   from './views/home/Home.vue'
+import List   from './views/list/index.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            meta: {title: '首页'},
+            component: Home
+        },
+        {
+            path: '/list',
+            name: 'index',
+            component: List
+        }
+    ]
 })
---
+++ src/views/list/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>list index</div>
+</template>
+
+<script>
+    export default {
+        name: 'index'
+    }
+</script>
+
+<style scoped>
+
+</style>
