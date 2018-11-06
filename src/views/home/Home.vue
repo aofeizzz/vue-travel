538fc3103ffc52d1f1657ae1fa7988644930465e
aofeizzz <228589060@qq.com>
2018-11-06 19:40:05 +0800

    'index-icons'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,16 +2,19 @@
     <div>
         <home-header></home-header>
         <home-swiper></home-swiper>
+        <home-icons></home-icons>
     </div>
 </template>
 
 <script>
     import HomeHeader from './components/home-header'
     import HomeSwiper from './components/home-swiper'
+    import HomeIcons  from './components/home-icons'
 
     export default {
         name: 'home',
         components: {
+            HomeIcons,
             HomeSwiper,
             HomeHeader
         },
---
+++ src/views/home/components/home-icons.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="icons">
+        <div class="icon" v-for="item of icons" :key="item.id">
+            <div class="icon-img">
+                <img class="icon-img-content" :src="item.img"/>
+            </div>
+            <p class="icon-title">{{item.title}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home-icons',
+        data () {
+            return {
+                icons: [
+                    {
+                        id: '001',
+                        title: '景点门票',
+                        img: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'
+                    },
+                    {
+                        id: '002',
+                        title: '北京必玩',
+                        img: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png'
+                    },
+                    {
+                        id: '003',
+                        title: '一日游',
+                        img: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png'
+                    },
+                    {
+                        id: '004',
+                        title: '文化古迹',
+                        img: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png'
+                    },
+                    {
+                        id: '005',
+                        title: '动植物园',
+                        img: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png'
+                    },
+                    {
+                        id: '006',
+                        title: '打卡圣地',
+                        img: 'https://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png'
+                    },
+                    {
+                        id: '007',
+                        title: '野生动物园',
+                        img: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png'
+                    },
+                    {
+                        id: '008',
+                        title: '泡温泉',
+                        img: 'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    @import "~@/assets/styles/varibles.styl"
+    .icons
+        overflow: hidden
+        height: 0
+        padding-bottom: 50%
+        .icon
+            position: relative
+            overflow: hidden
+            float: left
+            width: 25%
+            padding-bottom: 25%
+            .icon-img
+                position: absolute
+                top: 25%
+                left: 22%
+                right: 0
+                bottom: .44rem
+                width: 1.1rem
+                height: 1.1rem
+                box-sizing: border-box
+                .icon-img-content
+                    display: block
+                    margin: 0 auto
+                    height: 100%
+            .icon-title
+                position: absolute
+                left: 0
+                right: 0
+                bottom: 0
+                height: .44rem
+                line-height: .44rem
+                text-align: center
+                color: $darkTextColor
+
+</style>
