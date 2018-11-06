b7e463fadad7aed685021f0efb354233c54fbe99
aofeizzz <228589060@qq.com>
2018-11-06 22:39:42 +0800

    home-icons

--- src/views/home/components/home-icons.vue
+++ src/views/home/components/home-icons.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="icons">
-        <div class="icon" v-for="item of icons" :key="item.id">
-            <div class="icon-img">
-                <img class="icon-img-content" :src="item.img"/>
-            </div>
-            <p class="icon-title">{{item.title}}</p>
-        </div>
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="(page, index) of pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                        <img class="icon-img-content" :src="item.img"/>
+                    </div>
+                    <p class="icon-title">{{item.title}}</p>
+                </div>
+            </swiper-slide>
+        </swiper>
     </div>
 </template>
 
@@ -14,6 +18,9 @@
         name: 'home-icons',
         data () {
             return {
+                swiperOption: {
+                    autoplay: false
+                },
                 icons: [
                     {
                         id: '001',
@@ -54,46 +61,72 @@
                         id: '008',
                         title: '泡温泉',
                         img: 'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png'
+                    },
+                    {
+                        id: '009',
+                        title: '东部华侨城',
+                        img: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png'
+                    },
+                    {
+                        id: '010',
+                        title: '全部玩乐',
+                        img: 'http://img1.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png'
                     }
                 ]
+            }
+        },
+        computed: {
+            pages () {
+                const pages = []
+                this.icons.forEach((item, index) => {
+                    const page = Math.floor(index / 8)
+                    if (!pages[page])
+                        pages[page] = []
+                    pages[page].push(item)
+                })
+                return pages
             }
         }
     }
 </script>
 
+<style lang="stylus">
+    .icons
+        .swiper-container
+            height: 0
+            padding-bottom: 50%
+</style>
+
 <style lang="stylus" scoped>
     @import "~@/assets/styles/varibles.styl"
-    .icons
+    @import "~@/assets/styles/mixins.styl"
+    .icon
+        position: relative
         overflow: hidden
-        height: 0
-        padding-bottom: 50%
-        .icon
-            position: relative
-            overflow: hidden
-            float: left
-            width: 25%
-            padding-bottom: 25%
-            .icon-img
-                position: absolute
-                top: 25%
-                left: 22%
-                right: 0
-                bottom: .44rem
-                width: 1.1rem
-                height: 1.1rem
-                box-sizing: border-box
-                .icon-img-content
-                    display: block
-                    margin: 0 auto
-                    height: 100%
-            .icon-title
-                position: absolute
-                left: 0
-                right: 0
-                bottom: 0
-                height: .44rem
-                line-height: .44rem
-                text-align: center
-                color: $darkTextColor
-
+        float: left
+        width: 25%
+        padding-bottom: 25%
+        .icon-img
+            position: absolute
+            top: 25%
+            left: 22%
+            right: 0
+            bottom: .44rem
+            width: 1.1rem
+            height: 1.1rem
+            box-sizing: border-box
+            .icon-img-content
+                display: block
+                margin: 0 auto
+                height: 100%
+        .icon-title
+            position: absolute
+            left: 0
+            right: 0
+            bottom: 0
+            height: .44rem
+            line-height: .44rem
+            text-align: center
+            color: $darkTextColor
+            ellipsis()
 </style>
