a52e0aa06c48c9f8c2cb321469eb5bab06283032
aofeizzz <228589060@qq.com>
2018-11-09 01:25:51 +0800

    add better-scroll, city-list and city-alphabet component

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -2,16 +2,22 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
+        <city-alphabet></city-alphabet>
     </div>
 </template>
 
 <script>
-    import CityHeader from './components/city-header'
-    import CitySearch from './components/city-search'
+    import CityHeader   from './components/city-header'
+    import CitySearch   from './components/city-search'
+    import CityList     from './components/city-list'
+    import CityAlphabet from './components/city-alphabet'
 
     export default {
         name: 'city',
         components: {
+            CityAlphabet,
+            CityList,
             CitySearch,
             CityHeader
         }
@@ -19,4 +25,5 @@
 </script>
 
 <style lang="stylus" scoped>
+
 </style>
